9654650ae30b85de87c8579a033733938cf595e9
Ludovic Petrenko <Ludonope@users.noreply.github.com>
2024-04-13 01:25:30 +0200

    Layout and UI (#2)
    * Improved layout
    
    * Fixed little mistake
    
    * Implemented zoom with scrollwheel
    
    * Focus zoom on cursor position
    
    * Improved mobile layout, zoom and pan

--- script.js
+++ script.js
@@ -39,6 +39,11 @@ var maps = [
 var pins = [];
 var currentMap = "Monaco";
 var currentFile = "map_monaco.json";
+const zoomData = {
+    zoom: 1.0,
+    x: 0,
+    y: 0
+};
 
 let isAddPinMode = false;
 
@@ -314,11 +319,6 @@ function loadPins() {
     updateSidebar();
 }
 
-document.getElementById('sidebarToggle').addEventListener('click', function() {
-    var sidebar = document.getElementById('sidebar');
-    sidebar.style.display = sidebar.style.display === 'none' ? 'block' : 'none';
-});
-
 // Inject map choices in the menu.
 const mapChoices = document.getElementById('map-choices');
 for (let map of maps) {
@@ -341,17 +341,7 @@ for (let map of maps) {
     mapChoices.appendChild(choice);
 }
 
-const zoomRange = document.getElementById('zoomRange');
 const map = document.getElementById('map');
-
-zoomRange.addEventListener('input', function() {
-    map.style.transform = `scale(${this.value / 100})`;
-     // Adjust pin size based on map zoom level
-     var pins = document.getElementsByClassName('pin');
-     for (var i = 0; i < pins.length; i++) {
-         pins[i].style.transform = `scale(${1 / (this.value / 100)})`;
-     }
-});
 
 // Event Listeners
 
@@ -424,6 +414,106 @@ window.addEventListener('hashchange', loadMap);
 // Load map when the page loads
 window.onload = loadMap;
 
+let mapContainer = document.getElementById('map-container');
+const r = mapContainer.getBoundingClientRect();
+const min = Math.min(r.width, r.height);
+map.style.width = min + 'px';
+map.style.height = min + 'px';
+
+let observer = new ResizeObserver(function(mutations) {
+    const r = mapContainer.getBoundingClientRect();
+    const min = Math.min(r.width, r.height);
+    map.style.width = min + 'px';
+    map.style.height = min + 'px';
+});
+
+observer.observe(mapContainer);
+
+// Update CSS transforms for the map zoom and position.
+function updateZoom() {
+    const r = mapContainer.getBoundingClientRect();
+    const min = Math.min(r.width, r.height);
+    map.style.transform = `translate(${zoomData.x*min}px, ${zoomData.y*min}px) scale(${zoomData.zoom})`;
+    // Adjust pin size based on map zoom level
+    var pins = document.getElementsByClassName('pin');
+    for (var i = 0; i < pins.length; i++) {
+        pins[i].style.transform = `scale(${1 / zoomData.zoom})`;
+    }
+}
+
+function changeZoom(x, y, scale) {
+    const oldZoom = zoomData.zoom;
+    zoomData.zoom *= scale;
+
+    // Clamp zoom between a minimum and maximum value.
+    zoomData.zoom = Math.min(Math.max(0.5, zoomData.zoom), 8.0);
+    
+    // Apply translation to keep the zoom centered on the cursor.
+    zoomData.x -= (zoomData.zoom - oldZoom) * x;
+    zoomData.y -= (zoomData.zoom - oldZoom) * y;
+    
+    // Clamp translation to keep the zoomed map within the viewport.
+    zoomData.x = Math.min(Math.max(-zoomData.zoom, zoomData.x), zoomData.zoom);
+    zoomData.y = Math.min(Math.max(-zoomData.zoom, zoomData.y), zoomData.zoom);
+
+    updateZoom();
+}
+
+// Touchscreen events.
+const hammertime = new Hammer(mapContainer);
+hammertime.get('pinch').set({ enable: true });
+hammertime.get('pan').set({ enable: true });
+
+// Touchscreen zoom event handlers.
+let pinchData = {lastZoom: 1};
+hammertime.on('pinchstart', function(ev) {
+    const rect = map.getBoundingClientRect();
+    pinchData = {
+        x: (ev.center.x - rect.x) / rect.width,
+        y: (ev.center.y - rect.y) / rect.height,
+        lastZoom: 1,
+    };
+    pinchMove(ev);
+})
+hammertime.on('pinchmove', pinchMove)
+
+function pinchMove(ev) {
+    changeZoom(pinchData.x, pinchData.y, ev.scale/pinchData.lastZoom);
+    pinchData.lastZoom = ev.scale;
+}
+
+// Touchscreen move event handlers.
+let panData = {x: 0, y: 0};
+hammertime.on('panstart', function(ev) {
+    panData = {x: 0, y: 0};
+    panMove(ev);
+})
+hammertime.on('panmove', panMove)
+
+function panMove(ev) {
+    const rect = map.getBoundingClientRect();
+    zoomData.x += (ev.deltaX - panData.x) / rect.width * zoomData.zoom;
+    zoomData.y += (ev.deltaY - panData.y) / rect.height * zoomData.zoom;
+    panData = {x: ev.deltaX, y: ev.deltaY};
+    updateZoom();
+}
+
+// Mouse wheel zoom event handler.
+mapContainer.addEventListener('wheel', function(event) {
+    // Compute the cursor position as a percentage of the map size.
+    const rect = map.getBoundingClientRect();
+    const x = (event.clientX - rect.left) / rect.width;
+    const y = (event.clientY - rect.top) / rect.height;
+   
+    // Determine the direction of the scroll and adjust the zoom level.
+    const sign = event.deltaY < 0 ? -1 : 1;
+    changeZoom(x, y, 1 + sign * 0.05);
+});
+
+const filterPanel = document.getElementById('sidebar');
+function toggleFilter() {
+    filterPanel.classList.toggle('disabled');
+}
 
 
 
